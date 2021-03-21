618dd875c9ac741bd53374f2af35c26accfe6602
diego-garro <diego.garromolina@yahoo.com>
2021-03-20 21:30:37 -0600

    Add tables for state of runway in translations.dart

--- lib/src/database/translations.dart
+++ lib/src/database/translations.dart
@@ -96,6 +96,7 @@ class SKY_TRANSLATIONS {
     'WHT': 'white',
   };
 
+  // 3700
   Map<String, String> SEA_STATE = {
     '0': 'calm (glassy)',
     '1': 'calm (rippled)',
@@ -107,6 +108,66 @@ class SKY_TRANSLATIONS {
     '7': 'high',
     '8': 'very high',
     '9': 'phenomenal',
+  };
+
+  // 0919
+  Map<String, String> RUNWAY_DEPOSITS = {
+    '0': 'clear and dry',
+    '1': 'damp',
+    '2': 'wet and water patches',
+    '3': 'rime and frost covered (depth normally less than 1 mm)',
+    '4': 'dry snow',
+    '5': 'wet snow',
+    '6': 'slush',
+    '7': 'ice',
+    '8': 'compacted or rolled snow',
+    '9': 'frozen ruts or ridges',
+    '/':
+        'type of deposit not reported (e.g. due to runway clearance in progress),'
+  };
+
+  // 0519
+  Map<String, String> RUNWAY_CONTAMINATION = {
+    '1': 'less than 10%',
+    '2': '11%-25%',
+    '3': 'reserved',
+    '4': 'reserved',
+    '5': '26%-50%',
+    '6': 'reserved',
+    '7': 'reserved',
+    '8': 'reserved',
+    '9': '51%-100%',
+    '/': 'not reported',
+  };
+
+  // 1079
+  Map<String, String> DEPOSIT_DEPTH = {
+    '00': 'less than 1 mm',
+    '91': 'reserved',
+    '92': '10 cm',
+    '93': '15 cm',
+    '94': '20 cm',
+    '95': '25 cm',
+    '96': '30 cm',
+    '97': '35 cm',
+    '98': '40 cm or more',
+    '99':
+        'runway(s) non-operational due to snow, slush, ice, large drifts or runway clearance, but depth not reported',
+    '//': 'depth of deposit operationally not significant or not measurable',
+  };
+
+  // 0366
+  Map<String, String> SURFACE_FRICTION = {
+    '91': 'breaking action poor',
+    '92': 'breaking action medium/poor',
+    '93': 'breaking action medium',
+    '94': 'breaking action medium/good',
+    '95': 'breaking action good',
+    '96': 'reserved',
+    '97': 'reserved',
+    '98': 'reserved',
+    '99': 'unreliable',
+    '//': 'breaking conditions not reported and/or runway not operational',
   };
 
 // Translation of various remark codes into english

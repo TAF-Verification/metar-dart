b12ea2473cec7c364fcbfd94bc2aea04231df375
diego-garro <diego.garromolina@yahoo.com>
2021-09-23 20:54:56 -0600

    remove(metar): remove legacy code and units module

--- lib/src/units/units.dart
+++
@@ -1,7 +0,0 @@
-export 'package:metar_dart/src/units/angle.dart';
-export 'package:metar_dart/src/units/direction.dart';
-export 'package:metar_dart/src/units/length.dart';
-export 'package:metar_dart/src/units/position.dart';
-export 'package:metar_dart/src/units/pressure.dart';
-export 'package:metar_dart/src/units/speed.dart';
-export 'package:metar_dart/src/units/temperature.dart';

9a6804c7314d1975af9be80c4943cc8ae6b9e638
diego-garro <diego.garromolina@yahoo.com>
2022-06-26 15:58:08 -0600

    update example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -26,4 +26,30 @@ void main() {
   /// You can convert the METAR data as a String in JSON format with
   /// the toJSON() method
   print(metar.toJSON());
+
+  final taf = Taf('''
+    MROC 261738Z 2618/2718 25005KT 9999 SCT020 SCT120 TX27/2618Z TN19/2710Z
+          TEMPO 2618/2622 RA OVC070
+          TEMPO 2623/2703 3000 RA BR BKN005
+          BECMG 2704/2706 09002KT BCFG
+          BECMG 2714/2716 26006KT
+  ''');
+
+  /// Get the features of the TAF
+  /// Take care with posible null values depending of every report
+  print(taf.type_);
+  print(taf.station.name);
+  print(taf.time);
+  print(taf.wind.directionInDegrees);
+  print(taf.flightRules);
+
+  /// You can see a resume of the TAF with the toString() method
+  print(taf.toString());
+
+  /// You can convert the TAF data as a Map with the asMap() method
+  print(taf.asMap());
+
+  /// You can convert the TAF data as a String in JSON format with
+  /// the toJSON() method
+  print(taf.toJSON());
 }
